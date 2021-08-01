88b473a680b3ceb29bff102d1b686ec9d6fd668f
Ребриков Стефан/Rebrikov Stefan <robstef85@gmail.com>
2021-08-02 02:59:46 +0300

    Added Bugy Rarity Detect

--- RarityCheck.py
+++ RarityCheck.py
@@ -5,6 +5,52 @@ directory = 'Result/Success'
 files = Path(directory).glob('*.jpg')
 for file in files:
     img = cv2.imread(str(file), 1)
-    cv2.imshow('ss', img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    crop = img[20:50, 0:10]
+    # cv2.imshow('as', crop)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    rarity_directory = 'Rarity'
+    images = Path(rarity_directory).glob('*.jpg')
+    rar_dict = {}
+    for image in images:
+        rar_template = cv2.imread(str(image), 1)
+        rar_result = cv2.matchTemplate(img, rar_template, cv2.TM_CCOEFF_NORMED)
+        rar_min_val, rar_max_val, rar_min_loc, rar_max_loc = cv2.minMaxLoc(rar_result)
+        rar_dict[str(image).split('\\')[1][0:-4]] = float(str(rar_max_val)[0:4])
+    rar_dict_list = sorted(rar_dict.items(), key=lambda x: x[1], reverse=True)
+    print(str(file))
+    print(rar_dict)
+    rarity = rar_dict_list[0][0]
+    rarity_match = rar_dict_list[0][1]
+    if rarity != 'Ascended':
+        plus_crop = img[int(img.shape[0]*2/3):img.shape[0], 0:int(img.shape[1]/3)]
+        # cv2.imshow('ss', plus_crop)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        if rarity == 'Mythic':
+            plus_path = 'Rarity/PlusRarity/Mythic+.jpg'
+        elif rarity == 'Legendary':
+            plus_path = 'Rarity/PlusRarity/Legendary+.jpg'
+        elif rarity == 'Elite':
+            plus_path = 'Rarity/PlusRarity/Elite+.jpg'
+        plus_img = cv2.imread(plus_path, 1)
+        plus_result = cv2.matchTemplate(plus_crop, plus_img, cv2.TM_CCOEFF_NORMED)
+        plus_min_val, plus_max_val, plus_min_loc, plus_max_loc = cv2.minMaxLoc(plus_result)
+        print(plus_max_val)
+        if plus_max_val > 0.9:
+            rarity += '+'
+    print(rarity, rarity_match)
+
+
+
+
+
+# # Create crops for Rarity Templates
+# img = cv2.imread('Rarity/Mythic+.jpg', 1)
+# print(img.shape[0:2])
+# crop = img[59:68, 4:12]
+# cv2.imshow('ad', img)
+# cv2.imshow('ss', crop)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# cv2.imwrite('Rarity/Mythic+.jpg', crop)
--- Script.py
+++ Script.py
@@ -5,7 +5,7 @@ from pathlib import Path
 DefaultHeight = 960
 DefaultWidth = 432
 directory = 'HeroFacesDirectory/HeroFacesForTemplateMatching'
-filename = 'com.lilithgame.hgame.gp_Screenshot_2021.05.13_20.52.26.jpeg'  # Input HeroList
+filename = 'wGuamfjX26E.jpg'  # Input HeroList
 DefaultHeroList = cv2.imread(f'Herolist/{filename}', 1)
 Size = DefaultWidth / DefaultHeroList.shape[1]  # Default the Size of Herolist
 img = cv2.resize(DefaultHeroList, (0, 0), fx=Size, fy=Size)
@@ -48,10 +48,12 @@ def fraction_check(zone):
         fresult = cv2.matchTemplate(zone, fracimag, cv2.TM_CCOEFF_NORMED)
         f_min_val, f_max_val, f_min_loc, f_max_loc = cv2.minMaxLoc(fresult)
         fdict[str(frac).split('\\')[-1][0:-4]] = str(f_max_val)[0:4]
-    fdictlist = sorted(fdict.items(), key=lambda x: x[1], reverse=True)
+    f_dict_list = sorted(fdict.items(), key=lambda x: x[1], reverse=True)
     print(fdict, file=log)
     print(fdict)
-    return fdictlist[0][0]
+    print(f_dict_list[0][0], f_dict_list[0][1], file=log)
+    print(f_dict_list[0][0], f_dict_list[0][1])
+    return f_dict_list[0][0]
 
 
 def signature_check(zone):
@@ -67,6 +69,8 @@ def signature_check(zone):
     sidictlist = sorted(sidict.items(), key=lambda x: x[1], reverse=True)
     print(sidict, file=log)
     print(sidict)
+    print(sidictlist[0][0], sidictlist[0][1])
+    print(sidictlist[0][0], sidictlist[0][1], file=log)
     return sidictlist[0][0]
 
 
@@ -91,7 +95,42 @@ def furniture_check(zone):
     print(sort_furn_dict, file=log)
     if max_value < 0.92:
         furn_name = 'furn0'
+    print(furn_name, float(str(max_value)[0:4]))
+    print(furn_name, float(str(max_value)[0:4]), file=log)
+    print('')
     return furn_name
+
+
+def rarity_check(zone):
+    rarity_directory = 'Rarity'
+    images = Path(rarity_directory).glob('*.jpg')
+    rar_dict = {}
+    for image in images:
+        rar_template = cv2.imread(str(image), 1)
+        rar_result = cv2.matchTemplate(zone, rar_template, cv2.TM_CCOEFF_NORMED)
+        rar_min_val, rar_max_val, rar_min_loc, rar_max_loc = cv2.minMaxLoc(rar_result)
+        rar_dict[str(image).split('\\')[1][0:-4]] = float(str(rar_max_val)[0:4])
+    rar_dict_list = sorted(rar_dict.items(), key=lambda x: x[1], reverse=True)
+    print(rar_dict)
+    print(rar_dict, file=log)
+    rarity = rar_dict_list[0][0]
+    rarity_match = rar_dict_list[0][1]
+    if rarity != 'Ascended':
+        plus_crop = zone[int(zone.shape[0] * 2 / 3):zone.shape[0], 0:int(zone.shape[1] / 3)]
+        if rarity == 'Mythic':
+            plus_path = 'Rarity/PlusRarity/Mythic+.jpg'
+        elif rarity == 'Legendary':
+            plus_path = 'Rarity/PlusRarity/Legendary+.jpg'
+        elif rarity == 'Elite':
+            plus_path = 'Rarity/PlusRarity/Elite+.jpg'
+        plus_img = cv2.imread(plus_path, 1)
+        plus_result = cv2.matchTemplate(plus_crop, plus_img, cv2.TM_CCOEFF_NORMED)
+        plus_min_val, plus_max_val, plus_min_loc, plus_max_loc = cv2.minMaxLoc(plus_result)
+        if plus_max_val > 0.9:
+            rarity += '+'
+    print(rarity, rarity_match)
+    print(rarity, rarity_match, file=log)
+    return rarity
 
 
 with open('Result/Log.txt', 'w') as log:
@@ -101,16 +140,18 @@ with open('Result/Log.txt', 'w') as log:
         image_name = str(face).split("\\")[-1]
         # cv2.putText(crop2, str(val)[0:4], (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)  # Match value
         if val > 0.85:  # If matched successful
-            print(str(val)[0:4], str(face).split("\\")[-1])
-            print(str(val)[0:4], str(face).split("\\")[-1], file=log)
+            print(str(face).split("\\")[-1][0:-4], float(str(val)[0:4]))
+            print(str(face).split("\\")[-1][0:-4], float(str(val)[0:4]), file=log)
             # Text locations
-            text_loc = (LeftCorner[0], RightCorner[1])  # Hero name location
-            frac_text_loc = (LeftCorner[0], RightCorner[1] - 15)  # Hero fraction text location
+            text_loc = (LeftCorner[0] + 25, RightCorner[1])  # Hero name location
+            frac_text_loc = (LeftCorner[0] + 25, RightCorner[1] - 15)  # Hero fraction text location
             sign_text_loc = (LeftCorner[0] + 25, RightCorner[1] - 30)  # Hero signature text location
             furn_text_loc = (LeftCorner[0] + 25, RightCorner[1] - 45)  # Hero furniture text location
+            rar_text_loc = (LeftCorner[0], RightCorner[1] - 60)  # Hero furniture text location
             # Crop for template
             mod_check_crop = crop[0:int((crop.shape[0]) / 1.8), 0:int((crop.shape[1]) / 2.3)]  # Zone for mod check
             # hero mods check
+            rarity = rarity_check(crop)
             fraction = fraction_check(mod_check_crop)  # Check for fraction
             signature = signature_check(mod_check_crop)  # Check for signature
             furniture = furniture_check(mod_check_crop)  # Check for furniture
@@ -122,15 +163,15 @@ with open('Result/Log.txt', 'w') as log:
                 image_true_name = image_name[0:-5]
             # put Hero name on result jpg
             if image_true_name not in success_dict:
-                cv2.putText(img2, image_true_name, text_loc, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
                 cv2.imwrite(f'Result/Success/{image_name}', crop)
                 cv2.rectangle(img2, LeftCorner, RightCorner, 255, 1)
                 success_dict[image_true_name] = [fraction, signature, furniture]  # Add hero to dict
-                # Put Fraction on herolist
+                # Put Text on herolist
+                cv2.putText(img2, image_true_name, text_loc, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
                 cv2.putText(img2, fraction, frac_text_loc, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-                # Put Si on herolist
                 cv2.putText(img2, signature, sign_text_loc, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
                 cv2.putText(img2, furniture, furn_text_loc, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                cv2.putText(img2, rarity, rar_text_loc, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
         else:
             cv2.imwrite(f'Result/Failure/{image_name}', crop)
             failure_list.append(image_name[0:-4])
